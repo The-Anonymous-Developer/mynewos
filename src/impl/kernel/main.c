1032e7792f2b73cc2f665bbaa41beb999b2c02f5
The-Anonymous-Developer <95202769+The-Anonymous-Developer@users.noreply.github.com>
2022-10-19 12:17:46 +0530

    Kernel A03, Build 01A6

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -10,4 +10,8 @@ void kernel_main() {
      print_str("This version was compiled on: 18 October 2022");
      print_newline();
      print_str("VERSION: CLOSED ALPHA TESTING || BUILD 01A1");
+     print_newline();
+     print_newline();
+     print_set_color(PRINT_COLOR_LIGHT_GRAY, PRINT_COLOR_BLACK);
+     print_str("Kernel Roadblock! We are working on the kernel. Please be patient!");
 }
--- src/intf/print.h
+++ src/intf/print.h
@@ -26,3 +26,7 @@ void print_clear();
 void print_char(char character);
 void print_str(char* string);
 void print_set_color(uint8_t foreground, uint8_t background);
+
+
+/*Below will define keyboard keys*/
+
